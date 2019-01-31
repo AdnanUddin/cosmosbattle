fa8bffcbee2ac732fc96d859bd5987856e77f39c
Felipe Medeiros <felipe.medeiros@circle6.com>
2019-01-31 11:50:05 -0500

    First commit, enjoy...

---
+++ Assets/Scripts/PlanetController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetController : MonoBehaviour
+{
+    [SerializeField]
+    private bool hasAddOn;
+    private int numberOfUnits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.hasAddOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
