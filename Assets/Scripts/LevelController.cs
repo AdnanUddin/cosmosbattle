124ffad40f18bc0ae9fbc36cff17fbf40554563c
Adnan Uddin <adnan.uddin@circle6.com>
2019-01-31 15:05:47 -0500

    added level manager

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject planet;
+    private PlanetController planetController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.InitializePlanets();
+    }
+
+    private void InitializePlanets()
+    {
+        Instantiate(planet, Vector3.zero, Quaternion.identity);
+
+        this.planetController = planet.GetComponent<PlanetController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
