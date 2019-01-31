c0cf36e320496d81e2461545cafa7a8bb67cfcc8
Felipe Medeiros <felipe.medeiros@circle6.com>
2019-01-31 13:58:17 -0500

    2nd commit

--- Assets/Scripts/PlanetController.cs
+++ Assets/Scripts/PlanetController.cs
@@ -5,18 +5,70 @@ using UnityEngine;
 public class PlanetController : MonoBehaviour
 {
     [SerializeField]
-    private bool hasAddOn;
-    private int numberOfUnits;
+    private GameObject addOnObject;
+
+    [SerializeField]
+    private TextMesh numberOfUnitsLabel;
+
+    [SerializeField]
+    private bool useAddOn;
+
+    [SerializeField]
+    [Range(0.1f, 10f)]
+    private float spawnInterval = 1f;
+
+    private int numberOfUnits = 0;
+
+    private float lastUnitSpawn;
 
     // Start is called before the first frame update
     void Start()
     {
-        this.hasAddOn = false;
+        this.numberOfUnits = 0;
+        this.lastUnitSpawn = Time.time;
+        this.useAddOn = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (this.useAddOn)
+        {
+            addOnObject.SetActive(true);
+        }
+        else
+        {
+            addOnObject.SetActive(false);
+        }
+
+        this.UpdateNumberOfUnitsLabel();
+    }
+
+    void FixedUpdate()
+    {
+        if (!useAddOn)
+        {
+            this.UnitSpawnCheck();
+        }
+    }
+
+    void UnitSpawnCheck()
+    {
+        if ((this.spawnInterval) < (Time.time - this.lastUnitSpawn))
+        {
+            this.SpawnUnit();
+
+            this.lastUnitSpawn = Time.time;
+        }
+    }
+
+    void SpawnUnit()
+    {
+        this.numberOfUnits++;
+    }
+
+    void UpdateNumberOfUnitsLabel()
+    {
+        numberOfUnitsLabel.text = numberOfUnits.ToString();
     }
 }
