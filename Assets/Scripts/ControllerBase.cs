1fc795e14325fcc0c369e2312b20f7f316f68065
Adnan Uddin <adnan.uddin6@gmail.com>
2019-02-03 21:32:33 -0500

    fixed bugs

--- Assets/Scripts/ControllerBase.cs
+++ Assets/Scripts/ControllerBase.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 
 public abstract class ControllerBase : MonoBehaviour
 {
-    public event EventHandler Created;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        this.Create();
-        this.Created?.Invoke(this, EventArgs.Empty);
-    }
+    void Start() => this.Create();
+    
     public abstract void Create();
+
+    public virtual void  Destory()
+    {
+
+    }
 
 }
---
+++ Assets/Scripts/Levels/Helpers/LevelOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOptions
+{
+    public int Levels { get; set; }
+
+    public int PlanetCount { get; set; }
+
+}
